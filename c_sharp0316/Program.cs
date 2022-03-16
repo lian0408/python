1abff95a97d9fb179679eb326db95ab284dc1674
lian0408 <l.laness0408@gmail.com>
2022-03-16 16:13:43 +0800

    加入專案檔案。

---
+++ c_sharp0316/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp0316
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.Write("請輸入你的分數");
+            score = Convert.ToInt32(Console.ReadLine());
+            string result = score >= 60 ? "及格" : "不及格";
+            Console.WriteLine("這次你{0}",result);
+            Console.ReadLine();
+        }
+    }
+}
